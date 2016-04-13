d11b63c9c0ac1d0a034f7ae86de843845167053c
maickon <maickonmaickon@hotmail.com>
2016-04-12 23:03:21 -0300

    melhorias no arquivo de inicializacao

--- init.php
+++ init.php
@@ -1,5 +1,13 @@
 <?php
-require 'config.php';
+//troque o caminho abaixo pelo caminho onde se localiza o seu projeto.
+define('PATH_CONFIG','C:/xampp/htdocs/desafio-luxfacta/config.php');
+
+//este caminho deve ser alterado confoem a localizacao do projeto.
+if(file_exists(PATH_CONFIG)):
+	require PATH_CONFIG;
+else:
+	trigger_error('Por favor, altere o caminho atribuido a varável <b>PATH_CONFIG</b> na terceira linha do arquivo init.php.',256);
+endif;
 
 //carrega uma classe quando instanciada
 //por padrao o nome da pasta de cada classe
@@ -7,11 +15,14 @@ require 'config.php';
 
 function __autoload($class){
 	global $config;
-	if(file_exists($config['core_path'].strtolower($class).DIRECTORY_SEPARATOR.strtolower($class).'.class.php')):
-		require $config['core_path'].strtolower($class).DIRECTORY_SEPARATOR.strtolower($class).'.class.php';
-	elseif(file_exists('../'.$config['core_path'].strtolower($class).DIRECTORY_SEPARATOR.strtolower($class).'.class.php')):
-		require '../'.$config['core_path'].strtolower($class).DIRECTORY_SEPARATOR.strtolower($class).'.class.php';
+	$core_path = $config['base_path'].$config['core_path'].strtolower($class).DIRECTORY_SEPARATOR.strtolower($class).'.class.php';
+	if(file_exists($core_path)):
+		require $core_path;
+	else:
+		trigger_error('A classe '.$class.' não foi carregada com sucesso.',256);
 	endif;
 }
 
-$tag = new Tags;
+//objetos globais para o sistema
+$tag 	= new Tags;
+$form 	= new Form;
