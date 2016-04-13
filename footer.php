2d8213e9014fce7ecc973d44983fb4801ec6dafa
maickon <maickonmaickon@hotmail.com>
2016-04-12 23:02:13 -0300

    reorganizando require de js

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <?php
-		$tag->script('type="text/javascript" src="js/jquery-2.2.3.min.js"'); $tag->script;
- 		$tag->script('type="text/javascript" src="js/bootstrap.min.js"'); $tag->script;
- 		$tag->script('type="text/javascript" src="js/knockout-3.4.0.js"'); $tag->script;
-		$tag->script('type="text/javascript" src="js/index.js"'); $tag->sript;
+		$tag->script('type="text/javascript" src="'.$config['base_url'].$config['js_path'].'jquery-2.2.3.min.js"'); $tag->script;
+ 		$tag->script('type="text/javascript" src="'.$config['base_url'].$config['js_path'].'knockout-3.4.0.js"'); $tag->script;
+		$tag->script('type="text/javascript" src="'.$config['base_url'].$config['js_path'].'jquery_mask.js"'); $tag->sript;
+ 		$tag->script('type="text/javascript" src="'.$config['base_url'].$config['js_path'].'bootstrap.min.js"'); $tag->script;
+		$tag->script('type="text/javascript" src="'.$config['base_url'].$config['js_path'].'index.js"'); $tag->sript;
 	$tag->body;
 $tag->html;
