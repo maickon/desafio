bfe47200324f42241b9adc4c62c2c1374b5fec6b
maickon <maickonmaickon@hotmail.com>
2016-04-12 22:59:41 -0300

    atualizando configuracoes

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
-
+//configuracao do DB
 require 'db/database.php';
+//configuracao de urls
 require 'routes/routes.php';
+//configuracao de labels
+require 'labels/labels.php';
 
 //arquivo de configuracao
 $config = 
@@ -9,12 +12,16 @@ $config =
 	//configuracao do DB
 	'data_base' 	=> $data_base,
 	//configuracao das pastas
+	'base_url'		=> 'http://127.0.0.1/desafio-luxfacta/',
+	'base_path'		=> 'C:/xampp/htdocs/desafio-luxfacta/',
 	'core_path' 	=> 'core/',
 	'css_path' 		=> 'css/',
 	'db_path' 		=> 'db/',
 	'img_path' 		=> 'images/',
 	'js_path' 		=> 'js/',
 	'produtos' 		=> $routes,
+	//labels
+	'labels' 		=> $labels,
 	//nome do projeto
 	'project_name'	=> 'Projeto de Desafio'
 
