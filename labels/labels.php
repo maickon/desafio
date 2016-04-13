75f390da39645dd24eecdd7490b82c3e73bd3b6f
maickon <maickonmaickon@hotmail.com>
2016-04-12 23:04:19 -0300

    arquivo de configuracao de labels

---
+++ labels/labels.php
@@ -0,0 +1,15 @@
+<?php
+
+//configuracao dos nomes que aparecem no formulario
+//e dos nomes de campo no DB
+$labels = 
+	[
+		//labels para produtos view
+		'NOME' 			=> 'Nome',
+		'ESTOQUE'		=> 'Produtos em estoque',
+		'PRECO' 		=> 'Preço',
+		//labels para produtos view DB
+		'NOME_DB' 		=> 'nome',
+		'ESTOQUE_DB'	=> 'estoque',
+		'PRECO_DB' 		=> 'preco'
+	];
