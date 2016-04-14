ddd598028f89b076e6984e2be728aa636172be01
maickon <maickonmaickon@hotmail.com>
2016-04-13 23:20:46 -0300

    nova classe para exibir mensagens

---
+++ core/flashmsg/flashmsg.class.php
@@ -0,0 +1,39 @@
+<?php
+class Flashmsg{
+	
+	private $form;
+	private $css_class;
+	
+	function __construct($type, $msg){
+		$this->form = new Form();
+		
+		switch($type):
+			case 'success': 
+				$this->css_class = 'alert alert-success';
+				$this->alert($type, $msg);
+			break;
+			
+			case 'info': 
+				$this->css_class = 'alert alert-info'; 
+				$this->alert($type, $msg);
+			break;
+			
+			case 'warning': 
+				$this->css_class = 'alert alert-warning'; 
+				$this->alert($type, $msg);
+			break;
+			
+			case 'danger': 
+				$this->css_class = 'alert alert-danger';
+				$this->alert($type, $msg);
+			break;
+		endswitch;
+	}
+	
+	function alert($type, $msg){
+		$this->form->_div(['class'=>$this->css_class.' col-md-12', 'role'=>'alert']);
+			$this->form->link_to_close();
+			echo "<strong>{$type}!</strong> {$msg}";
+		$this->form->div_();
+	}
+}
