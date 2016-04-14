c808a7c3793411726d0bb9a7de421893b598dd07
maickon <maickonmaickon@hotmail.com>
2016-04-13 23:24:08 -0300

    configurando labels

--- labels/labels.php
+++ labels/labels.php
@@ -4,12 +4,36 @@
 //e dos nomes de campo no DB
 $labels = 
 	[
+		//botoes da tabela
+		'new'			=> 'Novo',
+		'update'		=> 'Atualizar',
+		'delete' 		=> 'Deletar',
+		'view'	 		=> 'Visualizar',
+		'back'			=> 'Voltar',
 		//labels para produtos view
-		'NOME' 			=> 'Nome',
-		'ESTOQUE'		=> 'Produtos em estoque',
-		'PRECO' 		=> 'Preço',
+		'produto' 		=> 'Produtos',
+		'id'			=> '#ID',
+		'nome' 			=> 'Nome',
+		'estoque'		=> 'Produtos em estoque',
+		'preco' 		=> 'Preço',
+		
 		//labels para produtos view DB
-		'NOME_DB' 		=> 'nome',
-		'ESTOQUE_DB'	=> 'estoque',
-		'PRECO_DB' 		=> 'preco'
+		'id_db'			=> 'id',
+		'nome_db' 		=> 'nome',
+		'estoque_db'	=> 'estoque_inicial',
+		'preco_db' 		=> 'preco',
+
+
+		//mensagens de alerta
+		'success_msg'	=> 'Registro cadastrado com sucesso',
+		'danger_msg'	=> 'Um erro ocorreu. Registro não pode ser cadastrado.',
+		'warning_msg'	=> 'Já existe um registro com este nome!',
+
+		//mensagens do modal
+		'modal_delete_msg' 		=> 'Apagar dados',
+		'modal_warning_msg1'	=> 'Você tem certeza que deseja deletar isto?',
+		'modal_warning_msg2'	=> 'Ao fazer isso você estará apagando estas informações de vez da base de dados.',
+		'modal_yes_msg'			=> 'Sim',
+		'modal_no_msg'			=> 'Não'
+
 	];
