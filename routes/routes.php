650da1f4a379c77a8511a4a3c3e07c4d307b40d3
maickon <maickonmaickon@hotmail.com>
2016-04-13 23:23:59 -0300

    configurando rotas

--- routes/routes.php
+++ routes/routes.php
@@ -7,8 +7,10 @@ $routes =
 	'produtos_path' 		=> 'view/produto/',
 	'produtos_new_path'		=> 'view/produto/new.php',
 	'produtos_edit_path'	=> 'view/produto/edit.php',
+	'produtos_view_path'	=> 'view/produto/view.php',
 	'produtos_delete_path'	=> 'view/produto/delete.php',
 	'produtos_new_file'		=> 'new.php',
 	'produtos_edit_file'	=> 'edit.php',
+	'produtos_view_file'	=> 'view.php',
 	'produtos_delete_file'	=> 'delete.php',
 ];
