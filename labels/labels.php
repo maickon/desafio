9641ddb083a66d12ed2463fbec49f264691b8b4f
maickon <maickonmaickon@hotmail.com>
2016-04-15 01:18:14 -0300

    novos labels

--- labels/labels.php
+++ labels/labels.php
@@ -6,15 +6,19 @@ $labels =
 	[
 		//botoes da tabela
 		'new'			=> 'Novo',
-		'update'		=> 'Atualizar',
-		'delete' 		=> 'Deletar',
-		'view'	 		=> 'Visualizar',
+		'update'		=> '<div class="icon-link">Editar</div>',
+		'delete' 		=> '<div class="icon-link">Deletar</div>',
+		'view'	 		=> '<div class="icon-link">Visualizar</div>',
 		'back'			=> 'Voltar',
+		'cancel'		=> 'Cancelar',
 		//labels para produtos view
 		'produto' 		=> 'Produtos',
+		'new_produto' 	=> 'Novo Produto',
+		'edit_produto' 	=> 'Editar Produto',
+		'view_produto' 	=> 'Visualizar Produto',
 		'id'			=> '#ID',
 		'nome' 			=> 'Nome',
-		'estoque'		=> 'Produtos em estoque',
+		'estoque'		=> 'Quantidade em estoque',
 		'preco' 		=> 'Preço',
 		
 		//labels para produtos view DB
@@ -26,6 +30,7 @@ $labels =
 
 		//mensagens de alerta
 		'success_msg'	=> 'Registro cadastrado com sucesso',
+		'update_msg' 	=> 'Registro atualizado com sucesso.',
 		'danger_msg'	=> 'Um erro ocorreu. Registro não pode ser cadastrado.',
 		'warning_msg'	=> 'Já existe um registro com este nome!',
 
@@ -34,6 +39,9 @@ $labels =
 		'modal_warning_msg1'	=> 'Você tem certeza que deseja deletar isto?',
 		'modal_warning_msg2'	=> 'Ao fazer isso você estará apagando estas informações de vez da base de dados.',
 		'modal_yes_msg'			=> 'Sim',
-		'modal_no_msg'			=> 'Não'
+		'modal_no_msg'			=> 'Não',
+		'modal_delete_msg1'		=> 'Você está prestes a deletar um produto. Este processo é irreversível.',
+		'modal_delete_msg2'		=> 'Você tem certeza disso?',
+		'modal_delete_confirm_msg' => 'Confirmar para deletar produto'
 
 	];
