ab8a2121be2263d46a054b5e3b4c6b873139420f
maickon <maickonmaickon@hotmail.com>
2016-04-15 01:17:48 -0300

    removendo carregamento js do footer

--- footer.php
+++ footer.php
@@ -1,9 +1,4 @@
 <?php
-		$tag->script('src="'.$config['base_url'].$config['js_path'].'jquery.min.js"'); $tag->script;
-    	$tag->script('src="'.$config['base_url'].$config['js_path'].'datatables.min.js"'); $tag->script;
-    	$tag->script('src="'.$config['base_url'].$config['js_path'].'jquery.validate.js"'); $tag->sript;
-    	$tag->script('src="'.$config['base_url'].$config['js_path'].'bootstrap-select.js"'); $tag->script;
-    	$tag->script('src="'.$config['base_url'].$config['js_path'].'knockout-3.4.0.js"'); $tag->script;
-    	$tag->script('src="'.$config['base_url'].$config['js_path'].'index.js"'); $tag->sript;
+    	//$tag->script('src="'.$config['base_url'].$config['js_path'].'index.js"'); $tag->script;
 	$tag->body;
 $tag->html;
