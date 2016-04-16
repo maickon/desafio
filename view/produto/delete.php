3844e63bc58d66cb1a25462c6dffc6fe7086729c
maickon <maickonmaickon@hotmail.com>
2016-04-16 01:06:36 -0300

    inplementacao do delete

---
+++ view/produto/delete.php
@@ -0,0 +1,14 @@
+<?php
+require '../../header.php';
+require '../helper.php';
+
+$produto = new Produto('');
+
+$delete_at_produto = $produto->select($produto->getTable(), null, [['id','=', $_GET['id'] ? $_GET['id'] : ' ']]);
+$delete = $produto->delete_data($delete_at_produto);
+
+if($delete == 1):
+	header("Location:".$config['base_url'].$config['produtos']['produtos_path'].'?status=del');
+else:
+	header("Location:".$config['base_url'].$config['produtos']['produtos_path'].'?status=error');
+endif;
