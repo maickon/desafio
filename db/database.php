07f5eec87da280189c36c500c23a696d9a71d582
maickon <maickonmaickon@hotmail.com>
2016-04-17 18:00:37 -0300

    alterando local do arquivo de DB

--- db/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Configuracao de suario e senha do banco de dados
-define('DB_USER', "root");		
-define('DB_PASS', "");	  		
-define('DB_NAME', "desafio"); 	
-define('DB_HOST', "localhost");
