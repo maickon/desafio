46ef570342d5daa9b525f7484ba25fa8741b69d3
maickon <maickonmaickon@hotmail.com>
2016-04-17 17:58:50 -0300

    melhorias na pagina principal

--- body.php
+++ body.php
@@ -1,20 +1,40 @@
 <?php
-$tag->div(['class'=>'container-fluid']);
-	$tag->div(['class'=>'row']);
-		$tag->div(['class'=>'col-md-12']);
-	  		$tag->h1();
-	    		$tag->printer($config['project_name']);
-	  		$tag->h1;
-	    	
-	    	$tag->a('href="'.$config['produtos']['produtos_new_path'].'"');
-		    	$tag->printer('Novo Produto');
-		    $tag->a;
-		    
-		    $tag->printer('|');
+$tag->div('class="container"');
+	$tag->div('class="row"');
+        $tag->div('class="col-lg-12 text-center"');
+			
+            $form->h1('Desafio Luxfacta - Módulo Produto');
+            $tag->p('class="lead"');
+                $tag->printer('Tecnologias utilizadas');
+            $tag->p;
+            $tecnologias = [
+                'Bootstrap', 
+                'DataTables Bootstrap', 
+                'Jquery', 
+                'Jquery DataTables', 
+                'DataTables Bootstrap',
+                'Jquery Validate',
+                'Jquery MaskMoney',
+                'PHP',
+                'Mysql',
+                'Xamp Control Panel',
+                'Server Apache'
+                ];
+            $tag->ul('class="list-unstyled"');
+                foreach($tecnologias as $value):
+                    $tag->li();
+                        $tag->printer($value);
+                    $tag->li;
+                endforeach;
+            $tag->ul;
 
-		    $tag->a('href="'.$config['produtos']['produtos_path'].'"');
-		    	$tag->printer('Listar Produtos');
-		    $tag->a;
-	  $tag->div;
-	$tag->div;
+            $tag->p('class="lead"');
+                $tag->printer('Código fonte do projeto:');
+                $tag->br();
+                $tag->a('href="https://github.com/maickon/desafio" target="blank"');
+                   $tag->printer('Projeto Desafio Luxfacta');
+                $tag->a;
+            $tag->p;
+        $tag->div;
+    $tag->div;
 $tag->div;
